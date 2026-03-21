7f05d2c5c94488b092c76563b499d203a4ebcb43
ChristianLuca2003 <Dariusluca1999@gmail.com>
2026-03-21 13:50:30 +0200

    Interactive Objects

--- Elevated Bitches/Assets/Scripts/InteractiveObject.cs
+++ Elevated Bitches/Assets/Scripts/InteractiveObject.cs
@@ -4,28 +4,17 @@ public class InteractiveObject : MonoBehaviour
 {
     
     public GameObject uiToActivate; 
-    public DialogueManager dialogueManager;
-    public GameObject player;
 
     
     public void Interact()
     {
-
-		player.GetComponentInChildren<CameraController>().enabled = !player.GetComponentInChildren<CameraController>().enabled;
-		player.GetComponent<PlayerMovement>().enabled = !player.GetComponent<PlayerMovement>().enabled;
-
-		if (dialogueManager != null)
-        {
-            
-            dialogueManager.StartDialogue();
-        }
-
         if (uiToActivate != null)
         {
             
             uiToActivate.SetActive(!uiToActivate.activeSelf);
 
-			if (uiToActivate.activeSelf)
+            
+            if (uiToActivate.activeSelf)
             {
                 Cursor.lockState = CursorLockMode.None; 
                 Cursor.visible = true;
